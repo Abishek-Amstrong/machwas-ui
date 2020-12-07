f84004cc3f3ce518af362a4c92b5757d0db81567
unknown <abishekamstrong@gmail.com>
2020-12-08 00:35:05 +0530

    landing page and registration page are completed

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,11 +2,21 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 
 import { AuthPage } from "./auth.page";
+import { RegisterComponent } from "./register/register.component";
 
 const routes: Routes = [
   {
     path: "",
+    redirectTo: "landing",
+    pathMatch: "full",
+  },
+  {
+    path: "landing",
     component: AuthPage,
+  },
+  {
+    path: "register",
+    component: RegisterComponent,
   },
 ];
 
