c009891f643ccdd74ecd0546f925bd536c7a397a
unknown <abishekamstrong@gmail.com>
2020-12-10 00:04:48 +0530

    added profile screen

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,20 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { ProfilePageRoutingModule } from './profile-routing.module';
+import { ProfilePageRoutingModule } from "./profile-routing.module";
 
-import { ProfilePage } from './profile.page';
+import { ProfilePage } from "./profile.page";
+import { AngularMaterialModule } from "../shared/angular-material.module";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    ProfilePageRoutingModule
+    ProfilePageRoutingModule,
+    AngularMaterialModule,
   ],
-  declarations: [ProfilePage]
+  declarations: [ProfilePage],
 })
 export class ProfilePageModule {}
--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from "@angular/core";
+import { IonSlides } from "@ionic/angular";
 
 @Component({
-  selector: 'app-profile',
-  templateUrl: './profile.page.html',
-  styleUrls: ['./profile.page.scss'],
+  selector: "app-profile",
+  templateUrl: "./profile.page.html",
+  styleUrls: ["./profile.page.scss"],
 })
 export class ProfilePage implements OnInit {
+  slideOpts: any;
+  @ViewChild("slider") slider: IonSlides;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    this.slideOpts = {
+      initialSlide: 0,
+      speed: 400,
+    };
   }
 
+  ngOnInit() {}
+
+  nextSlide() {
+    console.log(this.slider);
+    this.slider.slideNext();
+  }
+
+  previousSlide() {
+    this.slider.slidePrev();
+  }
 }
