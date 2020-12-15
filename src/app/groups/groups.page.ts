98954a5f07d50337d38f9ff9cd699c4be8dadf2c
unknown <abishekamstrong@gmail.com>
2020-12-16 01:05:44 +0530

    read contacts code changes

--- src/app/groups/groups.page.ts
+++ src/app/groups/groups.page.ts
@@ -18,115 +18,115 @@ export class GroupsPage implements OnInit {
   constructor() {
     // this.loadContacts();
     this.selectedSegment = "Friends";
-    this.contacts = [
-      {
-        contactId: 1,
-        displayName: "Abishek Amstrong",
-        emails: [],
-        phoneNumbers: ["8015386360"],
-      },
-      {
-        contactId: 2,
-        displayName: "Akash Dubey",
-        emails: [],
-        phoneNumbers: ["9874563210"],
-      },
-      {
-        contactId: 3,
-        displayName: "Sunita Gogoi",
-        emails: [],
-        phoneNumbers: ["9780210003"],
-      },
-      {
-        contactId: 1,
-        displayName: "Abishek Amstrong",
-        emails: [],
-        phoneNumbers: ["8015386360"],
-      },
-      {
-        contactId: 2,
-        displayName: "Akash Dubey",
-        emails: [],
-        phoneNumbers: ["9874563210"],
-      },
-      {
-        contactId: 3,
-        displayName: "Sunita Gogoi",
-        emails: [],
-        phoneNumbers: ["9780210003"],
-      },
-      {
-        contactId: 1,
-        displayName: "Abishek Amstrong",
-        emails: [],
-        phoneNumbers: ["8015386360"],
-      },
-      {
-        contactId: 2,
-        displayName: "Akash Dubey",
-        emails: [],
-        phoneNumbers: ["9874563210"],
-      },
-      {
-        contactId: 3,
-        displayName: "Sunita Gogoi",
-        emails: [],
-        phoneNumbers: ["9780210003"],
-      },
-      {
-        contactId: 1,
-        displayName: "Abishek Amstrong",
-        emails: [],
-        phoneNumbers: ["8015386360"],
-      },
-      {
-        contactId: 2,
-        displayName: "Akash Dubey",
-        emails: [],
-        phoneNumbers: ["9874563210"],
-      },
-      {
-        contactId: 3,
-        displayName: "Sunita Gogoi",
-        emails: [],
-        phoneNumbers: ["9780210003"],
-      },
-      {
-        contactId: 1,
-        displayName: "Abishek Amstrong",
-        emails: [],
-        phoneNumbers: ["8015386360"],
-      },
-      {
-        contactId: 2,
-        displayName: "Akash Dubey",
-        emails: [],
-        phoneNumbers: ["9874563210"],
-      },
-      {
-        contactId: 3,
-        displayName: "Sunita Gogoi",
-        emails: [],
-        phoneNumbers: ["9780210003"],
-      },
-    ];
+    // this.contacts = [
+    //   {
+    //     contactId: 1,
+    //     displayName: "Abishek Amstrong",
+    //     emails: [],
+    //     phoneNumbers: ["8015386360"],
+    //   },
+    //   {
+    //     contactId: 2,
+    //     displayName: "Akash Dubey",
+    //     emails: [],
+    //     phoneNumbers: ["9874563210"],
+    //   },
+    //   {
+    //     contactId: 3,
+    //     displayName: "Sunita Gogoi",
+    //     emails: [],
+    //     phoneNumbers: ["9780210003"],
+    //   },
+    //   {
+    //     contactId: 1,
+    //     displayName: "Abishek Amstrong",
+    //     emails: [],
+    //     phoneNumbers: ["8015386360"],
+    //   },
+    //   {
+    //     contactId: 2,
+    //     displayName: "Akash Dubey",
+    //     emails: [],
+    //     phoneNumbers: ["9874563210"],
+    //   },
+    //   {
+    //     contactId: 3,
+    //     displayName: "Sunita Gogoi",
+    //     emails: [],
+    //     phoneNumbers: ["9780210003"],
+    //   },
+    //   {
+    //     contactId: 1,
+    //     displayName: "Abishek Amstrong",
+    //     emails: [],
+    //     phoneNumbers: ["8015386360"],
+    //   },
+    //   {
+    //     contactId: 2,
+    //     displayName: "Akash Dubey",
+    //     emails: [],
+    //     phoneNumbers: ["9874563210"],
+    //   },
+    //   {
+    //     contactId: 3,
+    //     displayName: "Sunita Gogoi",
+    //     emails: [],
+    //     phoneNumbers: ["9780210003"],
+    //   },
+    //   {
+    //     contactId: 1,
+    //     displayName: "Abishek Amstrong",
+    //     emails: [],
+    //     phoneNumbers: ["8015386360"],
+    //   },
+    //   {
+    //     contactId: 2,
+    //     displayName: "Akash Dubey",
+    //     emails: [],
+    //     phoneNumbers: ["9874563210"],
+    //   },
+    //   {
+    //     contactId: 3,
+    //     displayName: "Sunita Gogoi",
+    //     emails: [],
+    //     phoneNumbers: ["9780210003"],
+    //   },
+    //   {
+    //     contactId: 1,
+    //     displayName: "Abishek Amstrong",
+    //     emails: [],
+    //     phoneNumbers: ["8015386360"],
+    //   },
+    //   {
+    //     contactId: 2,
+    //     displayName: "Akash Dubey",
+    //     emails: [],
+    //     phoneNumbers: ["9874563210"],
+    //   },
+    //   {
+    //     contactId: 3,
+    //     displayName: "Sunita Gogoi",
+    //     emails: [],
+    //     phoneNumbers: ["9780210003"],
+    //   },
+    // ];
   }
 
   ngOnInit() {}
 
-  // async loadContacts() {
-  //   if (isPlatform("android")) {
-  //     let permission = await Contacts.getPermissions();
-  //     if (!permission.granted) {
-  //       return;
-  //     }
-  //   }
+  async loadContacts() {
+    if (isPlatform("android")) {
+      let permission = await Contacts.getPermissions();
+      if (!permission.granted) {
+        return;
+      }
+    }
 
-  //   Contacts.getContacts().then((result) => {
-  //     console.log(result);
-  //     this.contacts = result.contacts;
-  //   });
-  // }
+    Contacts.getContacts().then((result) => {
+      console.log(result);
+      this.contacts = result.contacts;
+    });
+  }
 
   segmentChanged(ev: any) {
     this.selectedSegment = ev.detail.value;
